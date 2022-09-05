bf6dbc970ac4a9556bd7888afa7baee0521f6e83
anhkhoi1511619 <92618299+anhkhoi1511619@users.noreply.github.com>
2022-09-05 10:34:24 +0700

    Add new accesstoken to github

--- github_staff/Source/Enviroments/Enviroment.swift
+++ github_staff/Source/Enviroments/Enviroment.swift
@@ -11,8 +11,8 @@ class Enviroment {
     // Becasue Github will remove token when upload it to github
     // So I split git for this example
     static let token_part1 = "ghp_"
-    static let token_part2 = "P42OX3aULXTikPVCL"
-    static let token_part3 = "90SwUfhfyqMxw1zOhNl"
+    static let token_part2 = "Jg8NDAJd438tTu8io9Yp"
+    static let token_part3 = "Vkc3g5k3gz183oQp"
     static let token = token_part1 + token_part2 + token_part3
     
     static let host = "https://api.github.com"
